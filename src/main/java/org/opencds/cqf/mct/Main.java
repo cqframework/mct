bb2259bb0a0e4837fe95132acd3ca4c93e098d35
c-schuler <hoofschu@gmail.com>
2022-12-23 13:10:43 -0700

    Added pom, licensing languag, copyright, app package for ui, and updated gitignore

---
+++ src/main/java/org/opencds/cqf/mct/Main.java
@@ -0,0 +1,13 @@
+package org.opencds.cqf.mct;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
