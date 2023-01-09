f7ff123806c1d882dbe62f98daa9c68640d490c1
c-schuler <hoofschu@gmail.com>
2023-01-08 21:25:07 -0700

    Gather operation implementation and tests

---
+++ java/src/main/java/org/opencds/cqf/mct/MctApplication.java
@@ -0,0 +1,54 @@
+package org.opencds.cqf.mct;
+
+import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.rest.server.RestfulServer;
+import org.opencds.cqf.mct.api.GatherAPI;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.elasticsearch.ElasticsearchRestClientAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+
+@ServletComponentScan(basePackageClasses = { RestfulServer.class })
+@SpringBootApplication(exclude = { ElasticsearchRestClientAutoConfiguration.class })
+@Import({ MctConfig.class })
+public class MctApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MctApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(MctApplication.class);
+	}
+
+	@Autowired
+	AutowireCapableBeanFactory beanFactory;
+
+	@Bean
+	public ServletRegistrationBean<RestfulServer> hapiServletRegistration(RestfulServer restfulServer) {
+		ServletRegistrationBean<RestfulServer> servletRegistrationBean = new ServletRegistrationBean<>();
+		beanFactory.autowireBean(restfulServer);
+		servletRegistrationBean.setName("MCT servlet");
+		servletRegistrationBean.setServlet(restfulServer);
+		servletRegistrationBean.addUrlMappings("/mct/*");
+		servletRegistrationBean.setLoadOnStartup(1);
+
+		return servletRegistrationBean;
+	}
+
+	@Bean
+	public RestfulServer restfulServer() {
+		RestfulServer fhirServer = new RestfulServer(FhirContext.forR4Cached());
+		fhirServer.registerProvider(new GatherAPI(fhirServer.getFhirContext()));
+		return fhirServer;
+	}
+
+}
---
+++ java/src/main/java/org/opencds/cqf/mct/SpringContext.java
@@ -0,0 +1,26 @@
+package org.opencds.cqf.mct;
+
+import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpringContext implements ApplicationContextAware {
+
+   private static ApplicationContext context;
+
+   public static <T> T getBean(Class<T> beanClass) {
+      return context.getBean(beanClass);
+   }
+
+   @Override
+   public void setApplicationContext(@NotNull ApplicationContext context) throws BeansException {
+      setContext(context);
+   }
+
+   private static synchronized void setContext(ApplicationContext context) {
+      SpringContext.context = context;
+   }
+}
