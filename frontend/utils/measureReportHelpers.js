0e983efe1a6e9d1498b36dea8daf69bfbc2508a5
Dewar <dewar0019@gmail.com>
2023-01-10 19:30:18 -1000

    fix folder nesting and mock implement of gather (#3)

--- frontend/utils/measureReportHelpers.js
+++
@@ -1,6 +0,0 @@
-const extractDescription = (measureReport) => {
-  const extUrl = 'http://hl7.org/fhir/5.0/StructureDefinition/extension-MeasureReport.population.description';
-  return measureReport?.extension?.find((extension) => extension.url === extUrl)?.valueString;
-};
-
-export { extractDescription };
