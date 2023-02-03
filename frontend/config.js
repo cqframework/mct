85c790f91794dc7bb88c41a93f9c708051b2b799
Dewar Tan <dewar0019@gmail.com>
2023-02-02 21:29:02 -1000

    fix config base url

--- frontend/config.js
+++
@@ -1,5 +0,0 @@
-const baseUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:8088' : 'http://a913da9435453489fb73187d9b16edc5-1641411053.us-east-1.elb.amazonaws.com'
-
-export {
-  baseUrl
-}
