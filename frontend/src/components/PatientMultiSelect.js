6d1c834102867939ea848d4768be97836748568e
Dewar Tan <dewar0019@gmail.com>
2023-02-26 16:54:31 -1000

    Add loading indicator for patient select

--- frontend/src/components/PatientMultiSelect.js
+++ frontend/src/components/PatientMultiSelect.js
@@ -8,6 +8,7 @@ import Checkbox from '@mui/material/Checkbox';
 import ListItemText from '@mui/material/ListItemText';
 import { useDispatch, useSelector } from 'react-redux';
 import { inputSelection } from 'store/reducers/filter';
+import LoadingButton from '@mui/lab/LoadingButton';
 
 const ITEM_HEIGHT = 48;
 const ITEM_PADDING_TOP = 8;
@@ -20,7 +21,7 @@ const MenuProps = {
   }
 };
 
-const PatientMultiSelect = ({ patients }) => {
+const PatientMultiSelect = ({ patients = [] }) => {
   const dispatch = useDispatch();
   const { selectedPatients } = useSelector((state) => state.filter);
   const handleChange = (event) => {
@@ -35,29 +36,38 @@ const PatientMultiSelect = ({ patients }) => {
   return (
     <div>
       <FormControl sx={{ width: 300 }}>
-        <InputLabel id="patient-multiple-chip-label">Patients</InputLabel>
-        <Select
-          labelId="patient-multiple-chip-label"
-          id="patient-multiple-chip"
-          multiple
-          inputProps={{ autoFocus: true }}
-          value={selectedPatients}
-          onChange={handleChange}
-          input={<OutlinedInput id="select-multiple-chip" label="Patients" />}
-          renderValue={(selected) => `${selected.length} patients selected`}
-          MenuProps={MenuProps}
-        >
-          <MenuItem value={'select-all'}>
-            <Checkbox checked={selectedPatients.length === patientIds.length} />
-            <ListItemText primary={'Select All'} />
-          </MenuItem>
-          {patientIds?.map((patientId, index) => (
-            <MenuItem key={patientId + '_' + index} value={patientId}>
-              <Checkbox checked={selectedPatients.indexOf(patientId) > -1} />
-              <ListItemText primary={patientId} />
-            </MenuItem>
-          ))}
-        </Select>
+        {patients.length === 0 ? (
+          <LoadingButton loading sx={{ height: '41.13px' }} loadingPosition="start" variant="outlined">
+            Loading Patients...
+          </LoadingButton>
+        ) : (
+          <>
+            <InputLabel id="patient-multiple-chip-label">Patients</InputLabel>
+            <Select
+              labelId="patient-multiple-chip-label"
+              id="patient-multiple-chip"
+              multiple
+              inputProps={{ autoFocus: true }}
+              disabled={patientIds?.length === 0}
+              value={selectedPatients}
+              onChange={handleChange}
+              input={<OutlinedInput id="select-multiple-chip" label="Patients" />}
+              renderValue={(selected) => `${selected.length} patients selected`}
+              MenuProps={MenuProps}
+            >
+              <MenuItem value={'select-all'}>
+                <Checkbox checked={selectedPatients.length === patientIds.length} />
+                <ListItemText primary={'Select All'} />
+              </MenuItem>
+              {patientIds?.map((patientId, index) => (
+                <MenuItem key={patientId + '_' + index} value={patientId}>
+                  <Checkbox checked={selectedPatients.indexOf(patientId) > -1} />
+                  <ListItemText primary={patientId} />
+                </MenuItem>
+              ))}
+            </Select>
+          </>
+        )}
       </FormControl>
     </div>
   );
