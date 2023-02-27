15080507202a552d6390788fad8cdc3d6ba84a49
Dewar Tan <dewar0019@gmail.com>
2023-02-27 10:16:00 -1000

    Update local dev for setup

--- bin/setup_app_files.sh
+++ bin/setup_app_files.sh
@@ -4,4 +4,5 @@ set -e
 set -o pipefail
 
 DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-sed -i '' 's/localhost:8080/cqf-ruler:8080/g' ${DIR}/../java/src/main/resources/configuration/facilities/facilities-bundle.json
+sed -i '' 's/localhost:8080/cqf-ruler-a:8080/g' ${DIR}/../java/src/main/resources/configuration/facilities/facilities-bundle.json
+sed -i '' 's/localhost:8082/cqf-ruler-b:8080/g' ${DIR}/../java/src/main/resources/configuration/facilities/facilities-bundle.json
